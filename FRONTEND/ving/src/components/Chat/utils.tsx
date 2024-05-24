122ae9191741af24ffa3592805733d6452d31eb2
김상범 <oironese@naver.com>
2024-05-24 16:54:33 +0900

    fix: 리팩토링 폴더구조 설정

--- FRONTEND/ving/src/components/Chat/utils.tsx
+++
@@ -1,6 +0,0 @@
-export const getRandomColor = (): string => {
-  const hue = Math.floor(Math.random() * 360);
-  const saturation = Math.floor(Math.random() * 10) + 70;
-  const lightness = Math.floor(Math.random() * 20) + 70;
-  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-}
--- FRONTEND/ving/src/utils/formatNumber.tsx
+++
@@ -1,3 +0,0 @@
-export const formatNumber = (number: number): string => {
-  return new Intl.NumberFormat().format(number);
-};
